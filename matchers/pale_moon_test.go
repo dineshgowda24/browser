a8dc1301d976b5efe33b5f48f33363522e281284
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 01:01:31 +0400

    feat: add PaleMoon matcher

---
+++ matchers/pale_moon_test.go
@@ -0,0 +1,68 @@
+package matchers
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewPaleMoon(t *testing.T) {
+	Convey("Subject: #NewPaleMoon", t, func() {
+		Convey("It should return a new PaleMoon instance", func() {
+			So(NewPaleMoon(""), ShouldHaveSameTypeAs, &PaleMoon{})
+		})
+	})
+}
+
+func TestPaleMoonName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return PaleMoon", func() {
+			So(NewPaleMoon("").Name(), ShouldEqual, "Pale Moon")
+		})
+	})
+}
+
+func TestPaleMoonVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the version", func() {
+				ua := testUserAgents["pale-moon"]
+
+				So(NewPaleMoon(ua.Linux).Version(), ShouldEqual, "25.6.0")
+				So(NewPaleMoon(ua.Mac).Version(), ShouldEqual, "25.3.1")
+				So(NewPaleMoon(ua.Windows).Version(), ShouldEqual, "25.6.0")
+				So(NewPaleMoon(ua.Android).Version(), ShouldEqual, "25.4.1")
+			})
+		})
+
+		Convey("When the version is not matched", func() {
+			Convey("It should return default version", func() {
+				pm := NewPaleMoon(testUserAgents["chrome"].Linux)
+				So(pm.Version(), ShouldEqual, "0.0")
+			})
+		})
+	})
+}
+
+func TestPaleMoonMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches PaleMoon", func() {
+			Convey("It should return true", func() {
+				ua := testUserAgents["pale-moon"]
+
+				So(NewPaleMoon(ua.Linux).Match(), ShouldBeTrue)
+				So(NewPaleMoon(ua.Mac).Match(), ShouldBeTrue)
+				So(NewPaleMoon(ua.Windows).Match(), ShouldBeTrue)
+				So(NewPaleMoon(ua.Android).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match PaleMoon", func() {
+			Convey("It should return false", func() {
+				ua := testUserAgents["chrome"]
+
+				So(NewPaleMoon(ua.Linux).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
