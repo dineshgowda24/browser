bec92f1e1b0fafbeac1cd22c9d8ee02d9fb0b427
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 01:01:31 +0400

    feat: add PaleMoon matcher

---
+++ matchers/pale_moon.go
@@ -0,0 +1,30 @@
+package matchers
+
+type PaleMoon struct {
+	base
+}
+
+
+var (
+	paleMoonName          = "Pale Moon"
+	paleMoonVersionRegexp = []string{`PaleMoon/([\d.]+)`}
+	paleMoonMatchRegex    = []string{`PaleMoon`}
+)
+
+func NewPaleMoon(userAgent string) *PaleMoon {
+	return &PaleMoon{
+		base: newBase(userAgent),
+	}
+}
+
+func (p *PaleMoon) Name() string {
+	return paleMoonName
+}
+
+func (p *PaleMoon) Version() string {
+	return p.version(paleMoonVersionRegexp, 1)
+}
+
+func (p *PaleMoon) Match() bool {
+	return p.match(paleMoonMatchRegex)
+}
