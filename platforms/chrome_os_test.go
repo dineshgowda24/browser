f3ad87cb139889f97e4479bd170e158464a59934
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 15:38:06 +0400

    feat: add ChromeOS platform

---
+++ platforms/chrome_os_test.go
@@ -0,0 +1,61 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewChromeOS(t *testing.T) {
+	Convey("Subject: #NewChromeOS", t, func() {
+		Convey("It should return a new ChromeOS instance", func() {
+			So(NewChromeOS(""), ShouldHaveSameTypeAs, &ChromeOS{})
+		})
+	})
+}
+
+func TestChromeOSName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Chrome OS", func() {
+			So(NewChromeOS("").Name(), ShouldEqual, "Chrome OS")
+		})
+	})
+}
+
+func TestChromeOSVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the correct version", func() {
+				p := testPlatforms
+				So(NewChromeOS(p["chrome-os-armv7l"]).Version(), ShouldEqual, "7077.134.0")
+				So(NewChromeOS(p["chrome-os-armv7i"]).Version(), ShouldEqual, "6158.49.0")
+				So(NewChromeOS(p["chrome-os-armv8l"]).Version(), ShouldEqual, "8872.54.0")
+				So(NewChromeOS(p["chrome-os-x86-64"]).Version(), ShouldEqual, "7647.84.0")
+				So(NewChromeOS(p["chrome-os-i686"]).Version(), ShouldEqual, "6310.68.0")
+				So(NewChromeOS(p["chrome-os-aarch64"]).Version(), ShouldEqual, "9202.64.0")
+			})
+		})
+	})
+}
+
+func TestChromeOSMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches Chrome OS", func() {
+			Convey("It should return true", func() {
+				p := testPlatforms
+				So(NewChromeOS(p["chrome-os-armv7l"]).Match(), ShouldBeTrue)
+				So(NewChromeOS(p["chrome-os-armv7i"]).Match(), ShouldBeTrue)
+				So(NewChromeOS(p["chrome-os-armv8l"]).Match(), ShouldBeTrue)
+				So(NewChromeOS(p["chrome-os-x86-64"]).Match(), ShouldBeTrue)
+				So(NewChromeOS(p["chrome-os-i686"]).Match(), ShouldBeTrue)
+				So(NewChromeOS(p["chrome-os-aarch64"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match", func() {
+			Convey("It should return false", func() {
+				So(NewChromeOS(testPlatforms["android-10"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
