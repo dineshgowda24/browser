9c22bb26edd3bb686a8a29ab8e8d5bb5ee605c9c
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 14:09:38 +0400

    spec: add yml to load test platforms

---
+++ platforms/platforms_test.go
@@ -0,0 +1,43 @@
+package platforms
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+// testPlatforms is a map of user agent strings for each platform from the platforms.yml file
+var testPlatforms map[string]string
+
+// initTestPlatforms reads the platforms.yml file and
+// unmarshals it into the testPlatforms map.
+func initTestPlatforms() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("failed to get working directory: %v", err)
+	}
+
+	wd = strings.Split(wd, "/browser")[0]
+	yamlFile, err := os.ReadFile(fmt.Sprintf("%s/browser/assets/_test/platforms.yml", wd))
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+
+	var data map[string]string
+	if err := yaml.Unmarshal(yamlFile, &data); err != nil {
+		log.Fatalf("failed to unmarshal: %v", err)
+	}
+
+	testPlatforms = data
+}
+
+// TestMain is the entry point for running tests in this package.
+func TestMain(m *testing.M) {
+	initTestPlatforms()
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
