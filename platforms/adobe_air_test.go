ed6385b6cd24545e09a7e812d3492238647e209b
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 14:10:53 +0400

    feat: add AdobeAir platform

---
+++ platforms/adobe_air_test.go
@@ -0,0 +1,61 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewAdobeAir(t *testing.T) {
+	Convey("Subject: #NewAdobeAir", t, func() {
+		Convey("It should return a new AdobeAir instance", func() {
+			So(NewAdobeAir(""), ShouldHaveSameTypeAs, &AdobeAir{})
+		})
+	})
+}
+
+func TestAdobeAirName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return AdobeAir", func() {
+			aa := NewAdobeAir("")
+			So(aa.Name(), ShouldEqual, "Adobe AIR")
+		})
+	})
+}
+
+func TestAdobeAirVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the correct version", func() {
+				aa := testPlatforms["adobe-air"]
+
+				So(NewAdobeAir(aa).Version(), ShouldEqual, "18.0")
+			})
+		})
+
+		Convey("When the version is not matched", func() {
+			Convey("It should return default version", func() {
+				a := testPlatforms["android-10"]
+				So(NewAdobeAir(a).Version(), ShouldEqual, "")
+			})
+		})
+	})
+}
+
+func TestAdobeAirMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches AdobeAir", func() {
+			Convey("It should return true", func() {
+				aa := testPlatforms["adobe-air"]
+				So(NewAdobeAir(aa).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match", func() {
+			Convey("It should return false", func() {
+				a := testPlatforms["android-10"]
+				So(NewAdobeAir(a).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
