eb79d30f319466c6c46ce415b73b98b66d985d02
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 22:48:22 +0400

    feat: add Windows platform

---
+++ platforms/windows_test.go
@@ -0,0 +1,68 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewWindows(t *testing.T) {
+	Convey("Subject: #NewWindows", t, func() {
+		Convey("It should return a new Windows instance", func() {
+			So(NewWindows(testPlatforms["windows-xp"]), ShouldHaveSameTypeAs, &Windows{})
+		})
+	})
+}
+
+func TestWindowsName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Windows", func() {
+			w := NewWindows(testPlatforms["windows-xp"])
+			So(w.Name(), ShouldEqual, windowsName)
+		})
+	})
+}
+
+func TestWindowsVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the version", func() {
+
+				So(NewWindows(testPlatforms["windows-xp"]).Version(), ShouldEqual, "5.1")
+				So(NewWindows(testPlatforms["windows-vista"]).Version(), ShouldEqual, "6.0")
+				So(NewWindows(testPlatforms["windows-7"]).Version(), ShouldEqual, "6.1")
+				So(NewWindows(testPlatforms["windows-8"]).Version(), ShouldEqual, "6.2")
+				So(NewWindows(testPlatforms["windows-8-1"]).Version(), ShouldEqual, "6.3")
+				So(NewWindows(testPlatforms["windows-10"]).Version(), ShouldEqual, "10.0")
+			})
+		})
+
+		Convey("When the version is not matched", func() {
+			Convey("It should return default version", func() {
+
+				So(NewWindows(testPlatforms["linux"]).Version(), ShouldEqual, "0")
+			})
+		})
+	})
+}
+
+func TestWindowsMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches Windows", func() {
+			Convey("It should return true", func() {
+				So(NewWindows(testPlatforms["windows-xp"]).Match(), ShouldBeTrue)
+				So(NewWindows(testPlatforms["windows-vista"]).Match(), ShouldBeTrue)
+				So(NewWindows(testPlatforms["windows-7"]).Match(), ShouldBeTrue)
+				So(NewWindows(testPlatforms["windows-8"]).Match(), ShouldBeTrue)
+				So(NewWindows(testPlatforms["windows-8-1"]).Match(), ShouldBeTrue)
+				So(NewWindows(testPlatforms["windows-10"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match Windows", func() {
+			Convey("It should return false", func() {
+				So(NewWindows(testPlatforms["linux"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
