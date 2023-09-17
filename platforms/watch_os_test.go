995b6451ee482d1320f25d1b6c43809dd05c01d1
dineshgowda24 <dinesh240894@gmail.com>
2023-09-18 00:02:20 +0400

    feat: add WatchOS platform

---
+++ platforms/watch_os_test.go
@@ -0,0 +1,63 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewWatchOS(t *testing.T) {
+	Convey("Subject: #NewWatchOS", t, func() {
+		Convey("It should return a new WatchOS instance", func() {
+			So(NewWatchOS(""), ShouldHaveSameTypeAs, &WatchOS{})
+		})
+	})
+}
+
+func TestWatchOSName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return the correct name", func() {
+
+			So(NewWatchOS(testPlatforms["watch-3"]).Name(), ShouldEqual, "Apple Watch OS")
+		})
+	})
+}
+
+func TestWatchOSVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the correct version", func() {
+
+				So(NewWatchOS(testPlatforms["apple-watch-3"]).Version(), ShouldEqual, "5.2.1")
+				So(NewWatchOS(testPlatforms["apple-watch-4"]).Version(), ShouldEqual, "5.3")
+				So(NewWatchOS(testPlatforms["apple-watch-5"]).Version(), ShouldEqual, "6.0.1")
+				So(NewWatchOS(testPlatforms["apple-watch-6"]).Version(), ShouldEqual, "7.0")
+			})
+		})
+
+		Convey("When the version is not matched", func() {
+			Convey("It should return 0.0", func() {
+				So(NewWatchOS(testPlatforms["mac-os"]).Version(), ShouldEqual, "0.0")
+			})
+		})
+	})
+}
+
+func TestWatchOSMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches WatchOS", func() {
+			Convey("It should return true", func() {
+				So(NewWatchOS(testPlatforms["apple-watch-3"]).Match(), ShouldBeTrue)
+				So(NewWatchOS(testPlatforms["apple-watch-4"]).Match(), ShouldBeTrue)
+				So(NewWatchOS(testPlatforms["apple-watch-5"]).Match(), ShouldBeTrue)
+				So(NewWatchOS(testPlatforms["apple-watch-6"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match WatchOS", func() {
+			Convey("It should return false", func() {
+				So(NewWatchOS(testPlatforms["mac-os"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
