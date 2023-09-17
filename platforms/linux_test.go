74ba7df54a320a1baf608c34e4249c3c93ea5ea2
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 21:40:52 +0400

    feat: add Linux platform

---
+++ platforms/linux_test.go
@@ -0,0 +1,49 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewLinux(t *testing.T) {
+	Convey("Subject: #NewLinux", t, func() {
+		Convey("It should return a new Linux instance", func() {
+			So(NewLinux(""), ShouldHaveSameTypeAs, &Linux{})
+		})
+	})
+}
+
+func TestLinuxName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Linux", func() {
+			So(NewLinux("").Name(), ShouldEqual, "Generic Linux")
+		})
+	})
+}
+
+func TestLinuxVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the version", func() {
+				So(NewLinux(testPlatforms["linux"]).Version(), ShouldEqual, "0")
+			})
+		})
+	})
+}
+
+func TestLinuxMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches Linux", func() {
+			Convey("It should return true", func() {
+				So(NewLinux(testPlatforms["linux"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match Linux", func() {
+			Convey("It should return false", func() {
+				So(NewLinux(testPlatforms["firefox"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
