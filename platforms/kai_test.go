68f36c3cd2e4f66a340feabaa6da589d1d00e09b
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 21:34:20 +0400

    feat: add KaiOS platform

---
+++ platforms/kai_test.go
@@ -0,0 +1,57 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewKaiOS(t *testing.T) {
+	Convey("Subject: #NewKaiOS", t, func() {
+		Convey("It should return a new KaiOS instance", func() {
+			So(NewKaiOS(""), ShouldHaveSameTypeAs, &KaiOS{})
+		})
+	})
+}
+
+func TestKaiOSName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return KaiOS", func() {
+			So(NewKaiOS("").Name(), ShouldEqual, "Kai OS")
+		})
+	})
+}
+
+func TestKaiOSVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the version", func() {
+				So(NewKaiOS(testPlatforms["kai-os-1"]).Version(), ShouldEqual, "1.0")
+				So(NewKaiOS(testPlatforms["kai-os-2"]).Version(), ShouldEqual, "2.0")
+			})
+		})
+
+		Convey("When the version is not matched", func() {
+			Convey("It should return default version", func() {
+				So(NewKaiOS(testPlatforms["firefox"]).Version(), ShouldEqual, "")
+			})
+		})
+	})
+}
+
+func TestKaiOSMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches KaiOS", func() {
+			Convey("It should return true", func() {
+				So(NewKaiOS(testPlatforms["kai-os-1"]).Match(), ShouldBeTrue)
+				So(NewKaiOS(testPlatforms["kai-os-2"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match KaiOS", func() {
+			Convey("It should return false", func() {
+				So(NewKaiOS(testPlatforms["firefox"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
