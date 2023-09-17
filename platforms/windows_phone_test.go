6af840d2f2808f26bcb9c16e12350df47b0c255b
dineshgowda24 <dinesh240894@gmail.com>
2023-09-17 22:34:20 +0400

    feat: add WindowsPhone platform

---
+++ platforms/windows_phone_test.go
@@ -0,0 +1,57 @@
+package platforms
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewWindowsPhone(t *testing.T) {
+	Convey("Subject: #NewWindowsPhone", t, func() {
+		Convey("It should return a new WindowsPhone instance", func() {
+			So(NewWindowsPhone(""), ShouldHaveSameTypeAs, &WindowsPhone{})
+		})
+	})
+}
+
+func TestWindowsPhoneName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Windows Phone", func() {
+			So(NewWindowsPhone("").Name(), ShouldEqual, "Windows Phone")
+		})
+	})
+}
+
+func TestWindowsPhoneVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the version", func() {
+				wp := NewWindowsPhone(testPlatforms["windows-phone"])
+				So(wp.Version(), ShouldEqual, "10.0")
+			})
+		})
+
+		Convey("When the version is not matched", func() {
+			Convey("It should return default version", func() {
+				wp := NewWindowsPhone(testPlatforms["windows-ce"])
+				So(wp.Version(), ShouldEqual, "")
+			})
+		})
+	})
+}
+
+func TestWindowsPhoneMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches Windows Phone", func() {
+			Convey("It should return true", func() {
+				So(NewWindowsPhone(testPlatforms["windows-phone"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match Windows Phone", func() {
+			Convey("It should return false", func() {
+				So(NewWindowsPhone(testPlatforms["windows-ce"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
