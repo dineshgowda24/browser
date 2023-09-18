31933939bfc057f82b6cc8b54cafdc5260799938
dineshgowda24 <dinesh240894@gmail.com>
2023-09-18 14:24:10 +0400

    feat: add MicroMessenger matcher

---
+++ matchers/micro_messenger_test.go
@@ -0,0 +1,64 @@
+package matchers
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewMicroMessenger(t *testing.T) {
+	Convey("Subject: #NewMicroMessenger", t, func() {
+		Convey("It should return a new MicroMessenger instance", func() {
+			So(NewMicroMessenger(""), ShouldHaveSameTypeAs, &MicroMessenger{})
+		})
+	})
+}
+
+func TestMicroMessengerName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return MicroMessenger", func() {
+			s := NewMicroMessenger("")
+			So(s.Name(), ShouldEqual, "MicroMessenger")
+		})
+	})
+}
+
+func TestMicroMessengerVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("When the version is matched", func() {
+			Convey("It should return the version", func() {
+				s := testUserAgents["micro-messenger"]
+
+				So(NewMicroMessenger(s.Android).Version(), ShouldEqual, "6.2.5.51")
+				So(NewMicroMessenger(s.IOS).Version(), ShouldEqual, "6.2.3")
+				So(NewMicroMessenger(s.Linux).Version(), ShouldEqual, "7.0.1")
+				So(NewMicroMessenger(s.Mac).Version(), ShouldEqual, "2.3.24")
+				So(NewMicroMessenger(s.Windows).Version(), ShouldEqual, "6.5.2.501")
+			})
+		})
+	})
+}
+
+func TestMicroMessengerMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When user agent matches MicroMessenger", func() {
+			Convey("It should return true", func() {
+				s := testUserAgents["micro-messenger"]
+
+				So(NewMicroMessenger(s.Android).Match(), ShouldBeTrue)
+				So(NewMicroMessenger(s.IOS).Match(), ShouldBeTrue)
+				So(NewMicroMessenger(s.Linux).Match(), ShouldBeTrue)
+				So(NewMicroMessenger(s.Mac).Match(), ShouldBeTrue)
+				So(NewMicroMessenger(s.Windows).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When user agent does not match MicroMessenger", func() {
+			Convey("It should return false", func() {
+				s := testUserAgents["chrome"]
+
+				So(NewMicroMessenger(s.Linux).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
