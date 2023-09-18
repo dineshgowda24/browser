725cd88224ac04853230be94403434892353e128
dineshgowda24 <dinesh240894@gmail.com>
2023-09-18 14:57:53 +0400

    spec: add yml to load test devices

---
+++ devices/devices_test.go
@@ -0,0 +1,43 @@
+package devices
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+// testDevices is a map of user agent strings for each device from the devices.yml file
+var testDevices map[string]string
+
+// initTestDevices reads the devices.yml file and
+// unmarshals it into the testDevices map.
+func initTestDevices() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("failed to get working directory: %v", err)
+	}
+
+	wd = strings.Split(wd, "/browser")[0]
+	yamlFile, err := os.ReadFile(fmt.Sprintf("%s/browser/assets/_test/devices.yml", wd))
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+
+	var data map[string]string
+	if err := yaml.Unmarshal(yamlFile, &data); err != nil {
+		log.Fatalf("failed to unmarshal: %v", err)
+	}
+
+	testDevices = data
+}
+
+// TestMain is the entry point for running tests in this package.
+func TestMain(m *testing.M) {
+	initTestDevices()
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
