a2d1ddcea8e65b6f225f380a7c21be3d3ec6a336
dineshgowda24 <dinesh240894@gmail.com>
2023-09-18 15:16:23 +0400

    feat: add ps5 device

---
+++ devices/playstation_5_test.go
@@ -0,0 +1,40 @@
+package devices
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewPlayStation5(t *testing.T) {
+	Convey("Subject: #NewPlayStation5", t, func() {
+		Convey("It should return a new PlayStation5 instance", func() {
+			So(NewPlayStation5(""), ShouldHaveSameTypeAs, &PlayStation5{})
+		})
+	})
+}
+
+func TestPlayStation5Name(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return PlayStation 5", func() {
+			s := NewPlayStation5("")
+			So(s.Name(), ShouldEqual, "PlayStation 5")
+		})
+	})
+}
+
+func TestPlayStation5Match(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When the user agent matches", func() {
+			Convey("It should return true", func() {
+				So(NewPlayStation5(testDevices["ps5"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When the user agent does not match", func() {
+			Convey("It should return false", func() {
+				So(NewPlayStation5(testDevices["bb-playbook-1"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
