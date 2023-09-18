917f7dcf1ff9b7b850f34030ced4bb5d1946b0dd
dineshgowda24 <dinesh240894@gmail.com>
2023-09-18 16:35:15 +0400

    feat: add KindleFire device

---
+++ devices/kindle_fire_test.go
@@ -0,0 +1,41 @@
+package devices
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewKindleFire(t *testing.T) {
+	Convey("Subject: #NewKindleFire", t, func() {
+		Convey("It should return a new KindleFire instance", func() {
+			So(NewKindleFire(""), ShouldHaveSameTypeAs, &KindleFire{})
+		})
+	})
+}
+
+func TestKindleFireName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Kindle Fire", func() {
+			s := NewKindleFire("")
+			So(s.Name(), ShouldEqual, "Kindle Fire")
+		})
+	})
+}
+
+func TestKindleFireMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When the user agent matches", func() {
+			Convey("It should return true", func() {
+				So(NewKindleFire(testDevices["kindle-fire-1"]).Match(), ShouldBeTrue)
+				So(NewKindleFire(testDevices["kindle-fire-2"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When the user agent does not match", func() {
+			Convey("It should return false", func() {
+				So(NewKindleFire(testDevices["bb-playbook-1"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
