bdd302e1faf17c6ce8da178db8dcd1ca003e62c7
dineshgowda24 <dinesh240894@gmail.com>
2023-10-02 21:42:21 +0400

    feat: add default device matcher

---
+++ devices/unknown_test.go
@@ -0,0 +1,32 @@
+package devices
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewUnknown(t *testing.T) {
+	Convey("Subject: #NewUnknown", t, func() {
+		Convey("It should return a new Unknown instance", func() {
+			So(NewUnknown(""), ShouldHaveSameTypeAs, &Unknown{})
+		})
+	})
+}
+
+func TestUnknownName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Unknown", func() {
+			s := NewUnknown("")
+			So(s.Name(), ShouldEqual, "Unknown")
+		})
+	})
+}
+
+func TestUnknownMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("It should always return true", func() {
+			So(NewUnknown("").Match(), ShouldBeTrue)
+		})
+	})
+}
