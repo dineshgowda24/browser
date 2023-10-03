7dd21c82bfb717d5d4c39289c988d51687262a91
dineshgowda24 <dinesh240894@gmail.com>
2023-10-03 20:16:34 +0400

    feat: add Ipad device

---
+++ devices/ipad_test.go
@@ -0,0 +1,42 @@
+package devices
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewIpad(t *testing.T) {
+	Convey("Given a user agent string", t, func() {
+		So(NewIpad(testDevices["ipad-1"]), ShouldHaveSameTypeAs, &Ipad{})
+	})
+}
+
+func TestIpadName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return iPad", func() {
+			s := NewIpad("")
+			So(s.Name(), ShouldEqual, "iPad")
+		})
+	})
+}
+
+func TestIpadMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When the user agent matches", func() {
+			Convey("It should return true", func() {
+				So(NewIpad(testDevices["ipad-1"]).Match(), ShouldBeTrue)
+				So(NewIpad(testDevices["ipad-2"]).Match(), ShouldBeTrue)
+				So(NewIpad(testDevices["ipad-3"]).Match(), ShouldBeTrue)
+				So(NewIpad(testDevices["ipad-4"]).Match(), ShouldBeTrue)
+				So(NewIpad(testDevices["ipad-5"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When the user agent doesn't match", func() {
+			Convey("It should return false", func() {
+				So(NewIpad(testDevices["bb-playbook-1"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
