17b1d7075f9e3ac787980cd3cc6a70b5b9950772
dineshgowda24 <dinesh240894@gmail.com>
2023-10-03 21:54:24 +0400

    feat: add Weibo matcher

---
+++ matchers/weibo.go
@@ -0,0 +1,30 @@
+
+package matchers
+
+type Weibo struct {
+	base
+}
+
+var (
+	weiboName          = "Weibo"
+	weiboVersionRegexp = []string{`(?i)(?:__weibo__)([\d.]+)`}
+	weiboMatchRegexp   = []string{`__weibo__`}
+)
+
+func NewWeibo(userAgent string) *Weibo {
+	return &Weibo{
+		base: newBase(userAgent),
+	}
+}
+
+func (w *Weibo) Name() string {
+	return weiboName
+}
+
+func (w *Weibo) Version() string {
+	return w.version(weiboVersionRegexp, 1)
+}
+
+func (w *Weibo) Match() bool {
+	return w.match(weiboMatchRegexp)
+}
---
+++ matchers/weibo_test.go
@@ -0,0 +1,52 @@
+package matchers
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewWeibo(t *testing.T) {
+	Convey("Given a user agent string", t, func() {
+		So(NewWeibo(""), ShouldHaveSameTypeAs, &Weibo{})
+	})
+}
+
+func TestWeiboName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Weibo", func() {
+			s := NewWeibo("")
+			So(s.Name(), ShouldEqual, "Weibo")
+		})
+	})
+}
+
+func TestWeiboVersion(t *testing.T) {
+	Convey("Subject: #Version", t, func() {
+		Convey("It should return default version", func() {
+			ua := testUserAgents["weibo"]
+
+			So(NewWeibo(ua.Android).Version(), ShouldEqual, "6.5.0")
+			So(NewWeibo(ua.IOS).Version(), ShouldEqual, "10.9.2")
+		})
+	})
+}
+
+func TestWeiboMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When the user agent matches", func() {
+			Convey("It should return true", func() {
+				ua := testUserAgents["weibo"]
+				So(NewWeibo(ua.Android).Match(), ShouldBeTrue)
+				So(NewWeibo(ua.IOS).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When the user agent doesn't match", func() {
+			Convey("It should return false", func() {
+				ua := testUserAgents["safari"]
+				So(NewWeibo(ua.Android).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
