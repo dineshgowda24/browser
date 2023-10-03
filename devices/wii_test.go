fc5e13e0a72b1b3282da5e9f5558c22b205fc3a9
dineshgowda24 <dinesh240894@gmail.com>
2023-10-03 17:06:35 +0400

    feat: add Nintendo Wii device

---
+++ devices/wii_test.go
@@ -0,0 +1,40 @@
+package devices
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewWii(t *testing.T) {
+	Convey("Given a user agent string", t, func() {
+		So(NewWii(testDevices["wii-1"]), ShouldHaveSameTypeAs, &Wii{})
+	})
+}
+
+func TestWiiName(t *testing.T) {
+	Convey("Subject: #Name", t, func() {
+		Convey("It should return Wii", func() {
+			s := NewWii("")
+			So(s.Name(), ShouldEqual, "Nintendo Wii")
+		})
+	})
+}
+
+func TestWiiMatch(t *testing.T) {
+	Convey("Subject: #Match", t, func() {
+		Convey("When the user agent matches", func() {
+			Convey("It should return true", func() {
+				So(NewWii(testDevices["wii-1"]).Match(), ShouldBeTrue)
+				So(NewWii(testDevices["wii-2"]).Match(), ShouldBeTrue)
+				So(NewWii(testDevices["wii-3"]).Match(), ShouldBeTrue)
+			})
+		})
+
+		Convey("When the user agent does not match", func() {
+			Convey("It should return false", func() {
+				So(NewWii(testDevices["bb-playbook-1"]).Match(), ShouldBeFalse)
+			})
+		})
+	})
+}
